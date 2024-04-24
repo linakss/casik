d435fad7cfa52f88c8afc1b5fac926357efee595
linakss <linbuster444@gmail.com>
2024-04-24 14:31:00 +0300

    Initial commit

--- src/main/java/lin/online/casik/MainController.java
+++ src/main/java/lin/online/casik/MainController.java
@@ -20,6 +20,9 @@ import javafx.stage.Stage;
 public class MainController {
 
     @FXML
+    public Text txtAmountPobed;
+
+    @FXML
     private Text amountGame;
 
     @FXML
@@ -39,24 +42,29 @@ public class MainController {
 
     @FXML
     private Text txtProcentWin;
-    double game = 0;
+    double game = 0; //kol-vo igr
     double winGame = 0;
     double procentWin = 0;
     int max = 9, min = 0;
     String win = "7";
+    @FXML
+    private Text gamesAllPobed;
 
     @FXML private void initialize (){
         try {
             String winGames = Files.readString(Paths.get("procentWins.txt"));
-            String wining = Files.readString(Paths.get("winGames.txt"));
-
-            //procentWin = Double.parseDouble(winGames);
-            //winGame = Double.parseDouble(wining);
-
+            String wining = Files.readString(Paths.get("allWinGames.txt"));
+            String gamesAllStr = Files.readString(Paths.get("gamesAll.txt"));
+            procentWin = Double.parseDouble(winGames); //процент из файла
+            winGame = Double.parseDouble(wining); //кол-во побед из файла
+            game = Double.parseDouble(gamesAllStr); //кол-во круток из файла
             txtAmountGame.setVisible(false);
             txtProcentWin.setVisible(false);
             amountProcent.setVisible(false);
-            amountGame.setVisible(false);
+            amountGame.setVisible(false); //kol-vo pobed
+            txtAmountPobed.setVisible(false);
+            gamesAllPobed.setVisible(false);
+            gamesAllPobed.setText(String.valueOf(game));
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -64,8 +72,8 @@ public class MainController {
     }
     @FXML
     void btnPlay(ActionEvent event) throws IOException {
-        game++;
         Random rand = new Random();
+        game++;
 
         numberOne.setText(String.valueOf(rand.nextInt(max - min + 1) - min));
         numberTwo.setText(String.valueOf(rand.nextInt(max - min + 1) - min));
@@ -75,31 +83,37 @@ public class MainController {
         if (numberOne.getText().equals(win) || numberTwo.getText().equals(win) || numberThree.getText().equals(win)) {
             winGame++;
             MainApplication.showDialog("win-view.fxml","Счастливая семёрка!");
-        } 
+        }
 
+        gamesAllPobed.setText(String.valueOf(Math.round(winGame)));
         amountGame.setText(String.valueOf(Math.round(game)));
         procentWin = Math.round(winGame / game * 100);
         amountProcent.setText(String.valueOf(Math.round(procentWin)));
         
         PrintWriter fw1 = null;
         PrintWriter fw2 = null;
-        BufferedWriter bw1 = new BufferedWriter(fw1);
-        BufferedWriter bw2 = new BufferedWriter(fw2);
+        PrintWriter fw3 = null;
         try {
             fw1 = new PrintWriter("procentWins.txt");
-            fw2 = new PrintWriter("winGames.txt");
+            fw2 = new PrintWriter("allWinGames.txt");
+            fw3 = new PrintWriter("gamesAll.txt");
+            BufferedWriter bw1 = new BufferedWriter(fw1);
+            BufferedWriter bw2 = new BufferedWriter(fw2);
+            BufferedWriter bw3 = new BufferedWriter(fw3);
             bw1.write(amountGame.getText());
-            //bw2.write(procentWin.getText()); //не робит
-            //bw2.write(win.getText()); //не робит
+            bw2.write(amountProcent.getText());
+            bw3.write(gamesAllPobed.getText());
             bw1.close();
             fw1.close();
             bw2.close();
             fw2.close();
-
+            bw3.close();
+            fw3.close();
         } catch (IOException e) {
             e.printStackTrace();
             fw1.close();
             fw2.close();
+            fw3.close();
         }
     }
 
@@ -109,6 +123,8 @@ public class MainController {
         txtProcentWin.setVisible(true);
         amountProcent.setVisible(true);
         amountGame.setVisible(true);
+        txtAmountPobed.setVisible(true);
+        gamesAllPobed.setVisible(true);
     }
     @FXML
     void btnCancel(ActionEvent event) {
@@ -119,11 +135,6 @@ public class MainController {
 
 
 /*
-сколько всего нажатий
-сколько всего побед
-процент побед
-3 случайных числа
-данные должны изначально быть спрятаны
-когда нажмут на кнопку - показаться статистика
-
+сколько всего нажатий, процент побед - сохранять эти данные
+победное окно - открыть, закрыть по нажатию на кнопку
  */
--- src/main/java/lin/online/casik/WinContr.java
+++ src/main/java/lin/online/casik/WinContr.java
@@ -1,17 +1,27 @@
 package lin.online.casik;
 
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.stage.Stage;
-
-import java.util.ResourceBundle;
+        import javafx.event.ActionEvent;
+        import javafx.fxml.FXML;
+        import javafx.scene.control.Button;
+        import javafx.scene.image.ImageView;
+        import javafx.stage.Stage;
+        import javafx.scene.image.Image;
 
 public class WinContr {
     @FXML
+    public ImageView image;
+    @FXML
     private Button btnGoHome;
+
+
+    @FXML private void initialize (){
+
+        Image img = new Image(String.valueOf(MainApplication.class.getResource("pobeda.jpg")));
+        image.setImage(img);
+        image.setVisible(true);
+    }
     @FXML
     void btnGoHome(ActionEvent event) {
-
         Stage stage = (Stage) btnGoHome.getScene().getWindow();
         stage.close();
     }
